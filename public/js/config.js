31f1400cf2169e72a86982439310004db2f67fde
daniel <dduenas@tumipos.com>
2021-01-16 10:17:08 -0500

    F

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 window.config = {
-	"API":"http://voting-system.test:8080/api/v1/", //URL OF YOUR API LOCATED
-	"baseURL":"http://voting-system.test:8080/", //URL OF YOUR WEBSITE
-	"storageURL":"http://voting-system.test:8080/storage/", //URL WHERE YOUR IMAGES and OTHER FILEs stored
+	"API":"http://159.138.20.193:9200/api/v1/", //URL OF YOUR API LOCATED
+	"baseURL":"http://159.138.20.193:9200/", //URL OF YOUR WEBSITE
+	"storageURL":"http://159.138.20.193:9200/storage/", //URL WHERE YOUR IMAGES and OTHER FILEs stored
 	"debug":true
 }
 
